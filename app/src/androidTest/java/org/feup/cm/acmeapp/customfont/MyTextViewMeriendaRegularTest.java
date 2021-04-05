71679733e122e4089e2695a6e513b989ec2b9f87
jtmobs <jtmobs@icloud.com>
2021-04-05 18:57:49 +0100

    Made Several Changes, Testing the layout and the General Function of the App

---
+++ app/src/androidTest/java/org/feup/cm/acmeapp/customfont/MyTextViewMeriendaRegularTest.java
@@ -0,0 +1,37 @@
+package org.feup.cm.acmeapp.customfont;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import junit.framework.TestCase;
+
+public class MyTextViewMeriendaRegularTest extends TestCase {
+    public class MyTextView extends TextView {
+
+        public MyTextView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+            init();
+        }
+
+        public MyTextView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init();
+        }
+
+        public MyTextView(Context context) {
+            super(context);
+            init();
+        }
+
+        private void init() {
+            if (!isInEditMode()) {
+                Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/MavenPro-Regular.ttf");
+                setTypeface(tf);
+            }
+        }
+
+    }
+
+}
